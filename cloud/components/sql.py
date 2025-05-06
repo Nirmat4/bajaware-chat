3e888b2e84d58650baec026f80c6ae14604b5511
Nirmat4 <josafat.nova@outlook.com>
2025-05-06 15:59:32 -0600

    refactor: reestructuracion de proyecto

--- cloud/components/sql.py
+++ cloud/components/sql.py
@@ -8,37 +8,42 @@ from components.commons import db_path, prompt_sql, sql_model, muest_empt, table
 from rich import print
 conn=sqlite3.connect(db_path)
 
+history_prompt=[]
+history_table=[]
 def sql_search(prompt):
     choose=choose_table(prompt)
     if choose=="GENERALES":
-        print("DESC")
-        return muestreo, empty_message, "", ""
+        prompt=f"{history_prompt[-1]}{prompt}"
+        table=f"{history_table}"
+        print(f"[bold orange1]tabla:[/]\n[orange1]{table}[/orange1]")
+        text=prompt_sql(prompt, table)
     else:
         table=table_desc[choose]
         print(f"[bold orange1]tabla:[/]\n[orange1]{table}[/orange1]")
         text=prompt_sql(prompt, table)
-        # -- Generacion de consulta SQL --
-        stream=chat(
-            model=sql_model,
-            messages=[{
-                'role': 'user',
-                'content': text
-            }],
-            stream=True
-        )
-        response=""
-        for chunk in stream:
-            print(f"[cyan]{chunk['message']['content']}[/cyan]", end='', flush=True)
-            response+=(chunk['message']['content'])
-        print()
-        subprocess.run(['ollama', 'stop', sql_model])
-        time.sleep(1)
-        try:
-            query_result=pd.read_sql_query(response, conn)    
-            num_rows=len(query_result)
-            context=query_result.sample(n=min(10, num_rows), random_state=42)
-        except Exception as e:
-            print(f"[red]error en la ejecucion de la consulta: {e}[/red]")
-        muestreo, empty_message=muest_empt(num_rows, context)
-        
-        return muestreo, empty_message, response.replace("\n", ""), context.to_string(index=False)
+    stream=chat(
+        model=sql_model,
+        messages=[{
+            'role': 'user',
+            'content': text
+        }],
+        stream=True
+    )
+    response=""
+    for chunk in stream:
+        print(f"[cyan]{chunk['message']['content']}[/cyan]", end='', flush=True)
+        response+=(chunk['message']['content'])
+    print()
+    subprocess.run(['ollama', 'stop', sql_model])
+    time.sleep(1)
+    try:
+        query_result=pd.read_sql_query(response, conn)    
+        num_rows=len(query_result)
+        context=query_result.sample(n=min(10, num_rows), random_state=42)
+    except Exception as e:
+        print(f"[red]error en la ejecucion de la consulta: {e}[/red]")
+    muestreo, empty_message=muest_empt(num_rows, context)
+    history_prompt.append(f"{prompt}\n{response.replace("\n", "")}\n\n")
+    history_table.append(table)
+    
+    return muestreo, empty_message, response.replace("\n", ""), context.to_string(index=False)
